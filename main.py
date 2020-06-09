2901dfdb3949b894596752d870eacc47ac692c8a
🐔天パマスター🐔🌐 <tenpamaster108@gmail.com>
2020-06-09 12:17:07 +0900

    commit

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ def callback():
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,messages)
+    line_bot_api.reply_message(event.reply_token,messages=make_image_message())
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 def make_image_message():
     messages = ImageSendMessage(
