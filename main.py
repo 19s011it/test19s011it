211c810ff5f6df61f56c418a3e62975fbfd1c7d0
🐔天パマスター🐔🌐 <tenpamaster108@gmail.com>
2020-06-16 12:25:00 +0900

    commit

--- main.py
+++ main.py
@@ -27,7 +27,12 @@ def callback():
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
-    print(event)
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    with open(Path(f"static/images/{message_id}.jpg").absolute(), "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+
     line_bot_api.reply_message(event.reply_token,messages=make_image_message())
 
 @handler.add(MessageEvent, message=TextMessage)
@@ -35,10 +40,17 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 def make_image_message():
+    main_image_path = f"static/images/{message_id}_main.jpg"
+    preview_image_path = f"static/images/{message_id}_preview.jpg"
+    
     messages = ImageSendMessage(
-        original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
-        preview_image_url="https://hogehoge-mini.jpg"
+        original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+        preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
     )
+#    messages = ImageSendMessage(
+#        original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
+#        preview_image_url="https://hogehoge-mini.jpg"
+#    )
     return messages
 
 if __name__=="__main__":
