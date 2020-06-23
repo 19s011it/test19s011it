a3fbe5fdfca1c8d90c3da672781a903b4dfc4621
🐔天パマスター🐔🌐 <tenpamaster108@gmail.com>
2020-06-23 09:32:07 +0900

    commit

--- main.py
+++ main.py
@@ -34,25 +34,25 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 def make_image_message(event):
-    message_id = event.message.id
-    message_content = line_bot_api.get_message_content(message_id)
-
-    with open(Path(f"static/images/{message_id}.jpg").absolute(), "wb") as f:
-        for chunk in message_content.iter_content():
-            f.write(chunk)
-
-    main_image_path = f"static/images/{message_id}_main.jpg"
-    preview_image_path = f"static/images/{message_id}_preview.jpg"
-
+    # message_id = event.message.id
+    # message_content = line_bot_api.get_message_content(message_id)
+    #
+    # with open(Path(f"static/images/{message_id}.jpg").absolute(), "wb") as f:
+    #     for chunk in message_content.iter_content():
+    #         f.write(chunk)
+    #
+    # main_image_path = f"static/images/{message_id}_main.jpg"
+    # preview_image_path = f"static/images/{message_id}_preview.jpg"
+    #
+    # messages = ImageSendMessage(
+    #     original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+    #     preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
+    # )
+    # line_bot_api.reply_message(event.reply_token, messages )
     messages = ImageSendMessage(
-        original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
-        preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
+          original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
+          preview_image_url="https://hogehoge-mini.jpg"
     )
-    line_bot_api.reply_message(event.reply_token, messages )
-#    messages = ImageSendMessage(
-#        original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
-#        preview_image_url="https://hogehoge-mini.jpg"
-#    )
     return messages
 
 if __name__=="__main__":
