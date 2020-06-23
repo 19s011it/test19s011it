2dae2e92662b6fc07a6cacad6fd617e0b91ccd23
🐔天パマスター🐔🌐 <tenpamaster108@gmail.com>
2020-06-23 11:11:08 +0900

    commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, abort
+import os
+from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
-import os
+from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
+                            TextMessage, TextSendMessage)
 
 app=Flask(__name__)
 
@@ -29,32 +30,31 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,messages=make_image_message(event))
 
-@handler.add(MessageEvent, message=TextMessage)
+def make_image_message(event):
+    print("[CHECKPOINT]: {}".format(event))
+    # messages = ImageSendMessage(
+    #       original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
+    #       preview_image_url="https://hogehoge-mini.jpg"
+    # )
+    # return messages
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    with open("static/" + message_id + ".jpg", "wb") as f:
+        f.write(message_content.content)
+
+    image_message = ImageSendMessage(
+        #       original_content_url=URL + message_id+".jpg",
+        original_content_url=URL + "{}.jpg".format(message_id),
+        preview_image_url=URL + "{}.jpg".format(message_id),
+    )
+
+    line_bot_api.reply_message(event.reply_token, image_message)
+
+
+@handler.app(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-def make_image_message(event):
-    print("[CHECKPOINT]: {}".format(event))
-    # message_id = event.message.id
-    # message_content = line_bot_api.get_message_content(message_id)
-    #
-    # with open(Path(f"static/images/{message_id}.jpg").absolute(), "wb") as f:
-    #     for chunk in message_content.iter_content():
-    #         f.write(chunk)
-    #
-    # main_image_path = f"static/images/{message_id}_main.jpg"
-    # preview_image_path = f"static/images/{message_id}_preview.jpg"
-    #
-    # messages = ImageSendMessage(
-    #     original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
-    #     preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
-    # )
-    # line_bot_api.reply_message(event.reply_token, messages )
-    messages = ImageSendMessage(
-          original_content_url="https://upload.wikimedia.org/wikipedia/commons/0/0a/Kagoshima_Career_Design_College.JPG",
-          preview_image_url="https://hogehoge-mini.jpg"
-    )
-    return messages
 
 if __name__=="__main__":
     port = int(os.getenv("PORT",5000))
