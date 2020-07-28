61b9bd19860b500f2242e6470a349d158b97e994
🐔天パマスター🐔🌐 <tenpamaster108@gmail.com>
2020-07-28 13:39:55 +0900

    commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
                             TextMessage, TextSendMessage)
+from google.cloud import vision
+from google.oauth2 import service_account
 
 app=Flask(__name__)
 
@@ -13,6 +15,10 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 URL = "https://test19s011it.herokuapp.com/static/"
+credentials = service_account.Credentials.from_service_account_file(
+    './vision-api-dev-283300-2b166543073f.json'
+)
+client = vision.ImageAnnotatorClient(credentials=credentials)
 
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -40,10 +46,15 @@ def make_image_message(event):
     # return messages
     message_id = event.message.id
     message_content = line_bot_api.get_message_content(message_id)
+    
     with open("static/" + message_id + ".jpg", "wb") as f:
         f.write(message_content.content)
 
     image_url = URL + "{}.jpg".format(message_id)
+    response = client.annotate_image({
+    'image': {'source': {'image_uri': image_url}},
+    'features': [{'type': vision.enums.Feature.Type.FACE_DETECTION}],
+})
     image_message = ImageSendMessage(
         #       original_content_url=URL + message_id+".jpg",
         original_content_url=image_url,
